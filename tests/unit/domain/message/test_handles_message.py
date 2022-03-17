382d90d205caa80bbeb85b67e86c834ef854df81
Jongbeom-Kwon <bolk9652@naver.com>
2022-03-18 02:17:38 +0900

    Add test codes

--- tests/unit/domain/message/test_handles_message.py
+++ tests/unit/domain/message/test_handles_message.py
@@ -1,7 +1,7 @@
 from typing import Any
 
 from pydamain.domain.message.base import BaseMessage
-from pydamain.domain.message.main import handles_message
+from pydamain.domain.message.main import handle
 
 
 sync_flag = False
@@ -14,7 +14,7 @@ def sync_handler(m: "ExampleMessage", **_: Any):
     return "runned sync handler"
 
 
-def async_handler(m: "ExampleMessage", **_: Any):
+async def async_handler(m: "ExampleMessage", **_: Any):
     global async_flag
     async_flag = True
     return "runned async handler"
@@ -27,12 +27,12 @@ class ExampleMessage(BaseMessage):
 class TestHandleMessage:
     async def test_handle_sync(self):
         m = ExampleMessage()
-        result = await handles_message(m, sync_handler, {})
+        result = await handle(m, sync_handler, {})
         assert sync_flag
         assert result == "runned sync handler"
 
     async def test_handle_async(self):
         m = ExampleMessage()
-        result = await handles_message(m, async_handler, {})
+        result = await handle(m, async_handler, {})
         assert async_flag
         assert result == "runned async handler"
