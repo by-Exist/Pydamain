7ce757513ad22fff7d1f469e64f2d4e52756aa2d
Jongbeom-Kwon <bolk9652@naver.com>
2022-04-26 18:27:27 +0900

    Fix UnitOfWork protocol and Add NotInUOWContextError

--- src/pydamain/port/out_/unit_of_work.py
+++ src/pydamain/port/out_/unit_of_work.py
@@ -1,24 +1,32 @@
 from abc import abstractmethod
 from types import TracebackType
 from typing import Optional, Protocol
+from typing_extensions import Self
+
+
+class NotInUOWContextError(RuntimeError):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args, "can't call without uow context.")
 
 
 class UnitOfWork(Protocol):
-    async def __aenter__(self):
+    @abstractmethod
+    async def __aenter__(self) -> Self:
         return self
 
+    @abstractmethod
     async def __aexit__(
         self,
         exc_type: Optional[type[BaseException]],
         exc_value: Optional[BaseException],
         traceback: Optional[TracebackType],
-    ):
+    ) -> None:
         await self.rollback()
 
     @abstractmethod
-    async def commit(self):
-        ...
+    async def commit(self) -> None:
+        raise NotImplementedError()
 
     @abstractmethod
-    async def rollback(self):
-        ...
+    async def rollback(self) -> None:
+        raise NotImplementedError()
