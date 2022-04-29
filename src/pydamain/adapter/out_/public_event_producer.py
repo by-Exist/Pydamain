115d356245847e3194e892cbfce0297b8782a3ce
Jongbeom-Kwon <bolk9652@naver.com>
2022-04-29 18:49:19 +0900

    Fix producer method name (send -> produce)

--- src/pydamain/adapter/out_/public_event_producer.py
+++ src/pydamain/adapter/out_/public_event_producer.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass, field
-from typing import TYPE_CHECKING, ClassVar
-from uuid import UUID
+from typing import TYPE_CHECKING, ClassVar, Optional, cast
 
 from aiokafka import AIOKafkaProducer  # type: ignore
 from aiokafka.structs import RecordMetadata  # type: ignore
@@ -26,31 +25,32 @@ class BaseKafkaPublicEventProducer(PublicEventProducer):
             enable_idempotence=True,
         )
 
-    async def pre_send(self, msg: PublicEvent):
+    async def pre_produce(self, msg: PublicEvent):
         ...
 
-    async def post_send(self, msg: PublicEvent):
+    async def post_produce(self, msg: PublicEvent):
         ...
 
-    async def send(self, public_event: PublicEvent):
+    async def produce(self, public_event: PublicEvent):
         await self._aiokafka_producer.start()
-        await self.pre_send(public_event)
-        record_metadata: RecordMetadata = await self._aiokafka_producer.send_and_wait(  # type: ignore
-            topic=self.build_topic_name(public_event),
-            key=self.serialize_key(public_event.from_) if public_event.from_ else None,
-            value=self.serialize_value(public_event),
+        await self.pre_produce(public_event)
+        record_metadata = await self._aiokafka_producer.send_and_wait(  # type: ignore
+            topic=self.generate_topic_name(public_event),
+            key=self.generate_key(public_event),
+            value=self.generate_value(public_event),
         )
-        await self.post_send(public_event)
+        record_metadata = cast(RecordMetadata, record_metadata)
+        await self.post_produce(public_event)
         await self._aiokafka_producer.stop()
 
     @classmethod
-    def build_topic_name(cls, public_event: PublicEvent):
+    def generate_topic_name(cls, public_event: PublicEvent) -> str:
         return f"{cls.TOPIC_PREFIX}/{type(public_event).__name__}"
 
     @classmethod
-    def serialize_key(cls, id: UUID):
-        return id.bytes
+    def generate_key(cls, public_event: PublicEvent) -> Optional[bytes]:
+        return bytes(public_event.from_) if public_event.from_ else None
 
     @classmethod
-    def serialize_value(cls, public_event: PublicEvent):
+    def generate_value(cls, public_event: PublicEvent) -> bytes:
         return public_event.dumps()
--- src/pydamain/port/out_/public_event_producer.py
+++ src/pydamain/port/out_/public_event_producer.py
@@ -5,5 +5,5 @@ if TYPE_CHECKING:
 
 
 class PublicEventProducer(Protocol):
-    async def send(self, _msg: PublicEvent):
+    async def produce(self, _msg: PublicEvent):
         ...
