14227aadddbe7312c882e75edfedff70d4e22930
Jongbeom-Kwon <bolk9652@naver.com>
2022-04-29 16:52:36 +0900

    Fix outbox use PublicEvent(not Event)

--- src/pydamain/port/out_/outbox.py
+++ src/pydamain/port/out_/outbox.py
@@ -1,12 +1,11 @@
-from typing import TYPE_CHECKING, Any, Protocol
+from typing import Any, Protocol
 
 
-if TYPE_CHECKING:
-    from ...domain.messages import Event
+from ...domain.messages import PublicEvent
 
 
 class OutBox(Protocol):
-    async def set(self, event: Event) -> None:
+    async def set(self, event: PublicEvent) -> None:
         ...
 
     async def del_(self, id: Any) -> None:
