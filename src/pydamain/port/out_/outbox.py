f2125fe73f521952487a6819e3ee86b89b049f40
Jongbeom-Kwon <bolk9652@naver.com>
2022-05-09 16:10:28 +0900

    Outbox use Event(not PublicEvent)

--- src/pydamain/port/out_/outbox.py
+++ src/pydamain/port/out_/outbox.py
@@ -1,11 +1,11 @@
 from typing import Any, Protocol
 
 
-from ...domain.messages import PublicEvent
+from ...domain.messages import Event
 
 
 class OutBox(Protocol):
-    async def set(self, _event: PublicEvent) -> None:
+    async def set(self, _event: Event) -> None:
         ...
 
     async def del_(self, _id: Any) -> None:
