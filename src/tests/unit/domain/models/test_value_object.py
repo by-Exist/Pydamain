6cd3e61e4652e2d9efd4899c8886e9f29f27ad5b
Jongbeom-Kwon <bolk9652@naver.com>
2022-05-12 17:45:17 +0900

    Split value object test

--- src/tests/unit/domain/models/test_value_object.py
+++ src/tests/unit/domain/models/test_value_object.py
@@ -7,20 +7,16 @@ class Example(ValueObject):
     name: str
 
 
-class TestValueObject:
+def test_value_object_eq():
+    assert Example(name="foo") == Example(name="foo")
+    assert Example(name="foo") != Example(name="bar")
 
-    foo_1 = Example(name="foo")
-    foo_2 = Example(name="foo")
-    bar = Example(name="bar")
 
-    def test_value_object_eq(self):
-        assert self.foo_1 == self.foo_2
-        assert self.foo_1 != self.bar
+def test_value_object_hash():
+    assert len({Example(name="foo"), Example(name="foo")}) == 1
+    assert len({Example(name="foo"), Example(name="bar")}) == 2
 
-    def test_value_object_hash(self):
-        assert len({self.foo_1, self.foo_2}) == 1
-        assert len({self.foo_1, self.bar}) == 2
 
-    def test_value_object_frozen(self):
-        with pytest.raises(FrozenInstanceError):
-            self.foo_1.name = "other"
+def test_value_object_frozen():
+    with pytest.raises(FrozenInstanceError):
+        Example(name="foo").name = "other"
