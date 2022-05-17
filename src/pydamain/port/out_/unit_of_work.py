a8d6adb452a36f90f7da5471d52aabb130417947
Jongbeom-Kwon <bolk9652@naver.com>
2022-05-17 16:03:28 +0900

    Fix unit_of_work protocol

--- src/pydamain/port/out_/unit_of_work.py
+++ src/pydamain/port/out_/unit_of_work.py
@@ -1,17 +1,12 @@
-from abc import abstractmethod
-from dataclasses import dataclass
 from types import TracebackType
 from typing import Optional, Protocol
 from typing_extensions import Self
 
 
-@dataclass
 class UnitOfWork(Protocol):
-    @abstractmethod
     async def __aenter__(self) -> Self:
         ...
 
-    @abstractmethod
     async def __aexit__(
         self,
         exc_type: Optional[type[BaseException]],
@@ -20,10 +15,8 @@ class UnitOfWork(Protocol):
     ) -> None:
         ...
 
-    @abstractmethod
     async def commit(self) -> None:
         ...
 
-    @abstractmethod
     async def rollback(self) -> None:
         ...
