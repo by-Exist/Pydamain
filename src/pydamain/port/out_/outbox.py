f45a8dffc582a0818b819f51ff05e0a1ebc999e8
Jongbeom-Kwon <bolk9652@naver.com>
2022-05-19 14:39:20 +0900

    Remove outbox aggregate argument at put method

--- src/pydamain/port/out_/outbox.py
+++ src/pydamain/port/out_/outbox.py
@@ -2,11 +2,10 @@ from typing import Protocol, TypeVar
 
 
 EventType = TypeVar("EventType", contravariant=True)
-AggregateType = TypeVar("AggregateType", contravariant=True)
 
 
-class OutboxProtocol(Protocol[EventType, AggregateType]):
-    async def put(self, _event: EventType, _aggregate: AggregateType) -> None:
+class OutboxProtocol(Protocol[EventType]):
+    async def put(self, _event: EventType) -> None:
         ...
 
     async def delete(self, _event: EventType) -> None:
